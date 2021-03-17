5125c45ae7b608ed0a1c07f9316916b1ef680e2c
UO266618 <uo266618@uniovi.es>
2021-03-17 09:39:39 +0100

    Bug with menu and map view fixed

--- webapp/src/components/Map.js
+++ webapp/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 
 import 'here-js-api/styles/mapsjs-ui.css';
 
@@ -26,11 +26,12 @@ function addFriends(map,ui) {
 
 function Map() {
 
-    const [map, setMap] = useState(null);
+    const mapRef = useRef(null);
 
-    const [ui, setUi] = useState(null);
+    useLayoutEffect(() => {
 
-    const mapRef = useCallback((nodo) => {
+        if (!mapRef.current) return ;
+
         const H = window.H;
         const platform = new H.service.Platform({
             apikey: "s13roZogcqn6MI6dAT_MLNQ7PLMXbdmFMHSZfAEH1aI"
@@ -40,7 +41,7 @@ function Map() {
 
         // Create an instance of the map
         const map = new H.Map(
-            nodo,
+            mapRef.current,
             defaultLayers.vector.normal.map,
             {
                 // This map is centered over Europe
@@ -58,58 +59,52 @@ function Map() {
 
         // Create the default UI components to allow the user to interact with them
         // This variable is unused
-        setUi(H.ui.UI.createDefault(map, defaultLayers));
+        const ui = H.ui.UI.createDefault(map, defaultLayers);
+      
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position);
+            const H = window.H;
 
-        setMap(map);
+            //Resize of map in window
+            window.addEventListener("resize", () => map.getViewPort().resize());
 
-    }, [setMap]); //regenera mapref cada vez que cambie el map
-
-    useEffect(() => {
-        if (map) {
-            console.log(navigator.geolocation);
-            navigator.geolocation.getCurrentPosition((position) => {
-                console.log(position);
-                const H = window.H;
-
-                //Resize of map in window
-                window.addEventListener("resize", () => map.getViewPort().resize());
-
-                addFriends(map,ui);
+            addFriends(map, ui);
 
 
 
 
 
-                // Create a marker icon from an image URL:
-                var pngIcon = new H.map.Icon("/img/marker.png", { size: { w: 24, h: 24 } });
+            // Create a marker icon from an image URL:
+            var pngIcon = new H.map.Icon("/img/marker.png", { size: { w: 24, h: 24 } });
 
-                var LocationOfMarker = { lat: position.coords.latitude, lng: position.coords.longitude };
+            var LocationOfMarker = { lat: position.coords.latitude, lng: position.coords.longitude };
 
 
 
-                // Create a marker using the previously instantiated icon:
-                ///var marker = new H.map.Marker(LocationOfMarker, { icon: icon });  
-                var marker = new H.map.Marker(LocationOfMarker, { icon: pngIcon });
+            // Create a marker using the previously instantiated icon:
+            ///var marker = new H.map.Marker(LocationOfMarker, { icon: icon });  
+            var marker = new H.map.Marker(LocationOfMarker, { icon: pngIcon });
 
-                marker.addEventListener('tap', logEvent => {
-                    var bubble = new H.ui.InfoBubble({ lng: position.coords.longitude, lat: position.coords.latitude + 2 }, {
-                        content: '<b>Usuario A</b>'
-                    });
-                    ui.addBubble(bubble);
-                }, false);
-                // show info bubble
+            marker.addEventListener('tap', logEvent => {
+                var bubble = new H.ui.InfoBubble({ lng: position.coords.longitude, lat: position.coords.latitude + 2 }, {
+                    content: '<b>Usuario A</b>'
+                });
+                ui.addBubble(bubble);
+            }, false);
+            // show info bubble
 
-                // Add the marker to the map:
-                map.addObject(marker);
+            // Add the marker to the map:
+            map.addObject(marker);
 
-            }, (error) => {
-                console.error(error);
-            })
-            return () => {
-                map?.dispose();
-            }
+        }, (error) => {
+            console.error(error);
+        })
+        return () => {
+            map.dispose();
         }
-    }, [map]);
+
+
+    }, [mapRef]);
 
     return (
         // Set a height on the map so it will display
