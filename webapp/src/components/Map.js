c5c7445a134410ec218b70d75189bb1e18327aa5
UO266618 <uo266618@uniovi.es>
2021-03-31 11:23:41 +0200

    Marks modification

--- webapp/src/components/Map.js
+++ webapp/src/components/Map.js
@@ -1,13 +1,40 @@
-import React, { useRef, useLayoutEffect } from 'react';
-import { useWebId } from '@solid/react';
+import React, { useRef, useLayoutEffect, useEffect, useState, Fragment } from 'react';
+import { useLDflexValue, useWebId } from '@solid/react';
 
 import 'here-js-api/styles/mapsjs-ui.css';
+
+function MapMarker({webId, locationOfMarker, ui, map}){ 
+    const nombre = useLDflexValue("["+webId+"].name");
+	
+	useEffect(() => {
+		if (webId && nombre && locationOfMarker && ui && map) {
+            const H = window.H;
+			var pngIcon = new H.map.Icon("/img/marker.png", { size: { w: 24, h: 24 } });
+			var marker = new H.map.Marker(locationOfMarker, { icon: pngIcon });
+			map.addObject(marker);
+
+			marker.addEventListener('tap', logEvent => {
+				var bubble = new H.ui.InfoBubble({ lat: locationOfMarker.lat, lng: locationOfMarker.lng }, {
+					content: `${nombre}`
+				});
+				ui.addBubble(bubble);
+			}, false);
+		}
+	}, [webId, nombre, locationOfMarker, ui, map]);
+
+	
+	return null;
+}
 
 function Map() {
 
     const mapRef = useRef(null);
     // eslint-disable-next-line
     const solidId = useWebId();
+	const [marcas, setMarcas] = useState([]);
+	const [ui, setUI] = useState(null);
+	const [map, setMap] = useState(null);
+	const [userPosition, setUserPosition] = useState(null);
 
     // Default distanceRadius  5 km
     const distanceRadius = 5;
@@ -17,27 +44,29 @@ function Map() {
         var response = await respuesta.json();
         const H = window.H;
         var pngIcon = new H.map.Icon("/img/marker.png", { size: { w: 24, h: 24 } });
+        
+        //Borra la ubicación del usuario en sesión ELIMINAR
         map.removeObjects(map.getObjects())
 
+		var nuevasMarcas = [];
         // eslint-disable-next-line
         response.map((item, index) => {
-
+            
             if (distanceFilter(item.latitud, item.longitud, userPosition)) {
-                var LocationOfMarker = { lat: item.latitud, lng: item.longitud };
-                var marker = new H.map.Marker(LocationOfMarker, { icon: pngIcon });
-                map.addObject(marker);
-
-                marker.addEventListener('tap', logEvent => {
-                    var bubble = new H.ui.InfoBubble({ lat: item.latitud, lng: item.longitud }, {
-                        content: item.solidId
-                    });
-                    ui.addBubble(bubble);
-                }, false);
-
-                console.log(item)
+                var locationOfMarker = { lat: item.latitud, lng: item.longitud };
+				nuevasMarcas.push({
+					locationOfMarker,
+					webId: item.solidId
+				});
             }
         }
         )
+		
+		/*
+                var marker = new H.map.Marker(LocationOfMarker, { icon: pngIcon });
+                map.addObject(marker);
+		*/
+		setMarcas(nuevasMarcas);
     }
 
     // Calculates the distance between two coordinates according to Haversine Formule.
@@ -92,6 +121,7 @@ function Map() {
                 pixelRatio: window.devicePixelRatio || 1
             }
         );
+		setMap(map);
 
      
 
@@ -103,9 +133,11 @@ function Map() {
         // Create the default UI components to allow the user to interact with them
         // This variable is unused
         const ui = H.ui.UI.createDefault(map, defaultLayers);
+		setUI(ui);
 
         navigator.geolocation.getCurrentPosition((position) => {
             console.log(position);
+			setUserPosition({ lat: position.coords.latitude, lng: position.coords.longitude });
             const H = window.H;
 
             map.setCenter({lat: position.coords.latitude, lng: position.coords.longitude});
@@ -150,7 +182,23 @@ function Map() {
 
     return (
         // Set a height on the map so it will display
-        <div ref={mapRef} id="map" />
+        <Fragment>
+			<div ref={mapRef} id="map" />
+			{
+				marcas.map((marca, i) => <MapMarker 
+					key={`marca_${i}`}
+					webId={marca.webId}
+					locationOfMarker={marca.locationOfMarker}
+					ui={ui}
+					map={map}/>) 				
+			}
+
+			<MapMarker
+				webId={solidId}
+				locationOfMarker={userPosition}
+				ui={ui}
+				map={map}/>
+		</Fragment>
     );
 }
 
