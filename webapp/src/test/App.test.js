cdae2df9c69f212686040aec4e89e1e32309af8f
Taso <uo251434@uniovi.es>
2021-04-24 18:36:57 +0200

    prueba test warning

--- webapp/src/test/App.test.js
+++ webapp/src/test/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+//import App from "../App";
 
 test ("check that everything is rendering propertly", async () => {
   //const { getByText } = render(<App/>);
--- webapp/src/test/profile.test.js
+++ webapp/src/test/profile.test.js
@@ -6,13 +6,36 @@ import ProfileView from "../components/layouts/ProfileView";
 import { BrowserRouter } from 'react-router-dom';*/
 
 import React from "react";
-import {render, fireEvent, cleanup, pwgetByText}  from "@testing-library/react";
+import {render, screen, fireEvent, cleanup, pwgetByText, waitFor}  from "@testing-library/react";
 import 'globalthis/auto';
 import ReactDOM from 'react-dom';
 import ProfileView from "../components/layouts/ProfileView";
+import UserProfile from "../components/user/UserProfile";
+import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom';
+import "@testing-library/jest-dom/extend-expect";
+import App from '../App';
+import { act } from 'react-dom/test-utils';
+
+
 
 
 test("user can view its profile", async () => {
+
+    render(<UserProfile />);
+    
+    //expect(getByText("Meal")).toBeInTheDocument();
+    await waitFor(() => {
+        const element = screen.getByText("Meal");
+        expect(element).toBeInTheDocument();
+        const element1 = screen.getByText("Sport");
+        expect(element1).toBeInTheDocument();
+        const element2 = screen.getByText("Date");
+        expect(element2).toBeInTheDocument();
+        const element3 = screen.getByText("Unspecified");
+        expect(element3).toBeInTheDocument();
+});
+
 /*
     const { getAllByRole, getByPla ceholderText } = render(<BrowserRouter><Provider store={store}><LoginPage/></Provider></BrowserRouter>);
 
