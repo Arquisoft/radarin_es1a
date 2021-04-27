2b30ceb331e80b58e80b4102bf667edb2cc6d0b6
Taso <uo251434@uniovi.es>
2021-04-27 17:38:15 +0200

    test welcome

--- webapp/src/test/Welcome.test.js
+++ webapp/src/test/Welcome.test.js
@@ -2,11 +2,11 @@ import React from "react";
 import {render, screen, fireEvent, cleanup, pwgetByText, waitFor}  from "@testing-library/react";
 import 'globalthis/auto';
 import ReactDOM from 'react-dom';
-import Creators from "../components/menus/Creators";
+import Welcome from "../components/menus/Welcome";
 import "@testing-library/jest-dom/extend-expect";
 
 test("MenuBar render properly", async () => {
 
-   render(<Creators />);  
+   render(<Welcome />);  
 
 });
