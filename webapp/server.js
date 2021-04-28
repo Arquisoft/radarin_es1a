e6e06c7590060f5b5b80e69311bb6b95bfbe4599
Miguel <UO247134@uniovi.es>
2021-04-28 19:05:36 +0200

    Added code To server to solution GetPetitions

--- webapp/server.js
+++ webapp/server.js
@@ -4,7 +4,9 @@ var port =  process.env.PORT || 3000;
 app.use(express.static("build"));
 app.listen(port);
 
-
+app.get('*' , (req, res) => {
+    res.sendFile(path.join(__dirname,'build','index.html')); 
+});
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
