1199e85875b3a33665400d404a36b449ac8673b1
Taso <uo251434@uniovi.es>
2021-04-28 16:35:23 +0200

    Añadidos test

--- webapp/src/test/FriendList.test.js
+++ webapp/src/test/FriendList.test.js
@@ -3,12 +3,18 @@ import {render, screen, fireEvent, cleanup, pwgetByText, waitFor}  from "@testin
 import 'globalthis/auto';
 import ReactDOM from 'react-dom';
 import FriendList from "../components/friends/FriendList";
+import {FriendCardList} from "../components/friends/FriendList";
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 import "@testing-library/jest-dom/extend-expect";
+import { GetUserWebId, useGetUserFriends } from "../components/user/SolidManager";
 
-test("check that the list of users renders propertly", async () => {
+test("check that the list renders properly without any friends", async () => {
    
+   console.log(GetUserWebId());
+   await waitFor(() => {
+      console.log(GetUserWebId());
+   });
    render(<FriendList />);
     
    //await waitFor(() => {
