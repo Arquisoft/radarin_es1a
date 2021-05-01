7a6b032aa7bd716d87e30fdc18b47f0a44536adb
Miguel <UO247134@uniovi.es>
2021-05-01 19:37:07 +0200

    Added Test por App.js

--- webapp/src/test/App.test.js
+++ webapp/src/test/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {render, screen, fireEvent, cleanup, pwgetByText, waitFor}  from "@testing-library/react";
+import {render, screen, fireEvent, cleanup, pwgetByText, waitFor, getByAltText}  from "@testing-library/react";
 import "globalthis/auto";
 import ReactDOM from "react-dom";
 import Login from "../components/login/Login.js";
@@ -7,11 +7,17 @@ import "@testing-library/jest-dom/extend-expect";
 import App from "../App";
 
 test ("check that everything is rendering propertly", async () => {
-  render(<App/>);
-  /*
-  await waitFor(() => {
-    const element = screen.getByText("Radarin");
-    expect(element).toBeInTheDocument();
-  });*/
-  //expect(getByText("Home")).toBeInTheDocument();
+  const app = render(<App/>);
+  
+  await waitFor(() => {  
+    expect(app.container).toHaveTextContent("Radarin");
+    expect(app.container).toHaveTextContent("Watch the trailer");
+    expect(app.container).toHaveTextContent("University of Oviedo - Software Architecture");
+    expect(screen.getByAltText("radarin")).toBeInTheDocument();
+    expect(screen.getByAltText("functionalities")).toBeInTheDocument();
+    expect(screen.getByAltText("meeting")).toBeInTheDocument();
+    expect(screen.getByAltText("usability")).toBeInTheDocument();
+    expect(screen.getByAltText("principal")).toBeInTheDocument();
+ 
+  });
 });
