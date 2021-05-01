07ae0689c7790fad48e08f870c395a303363cde1
Taso <uo251434@uniovi.es>
2021-05-02 01:25:19 +0200

    Test MapMarker

--- webapp/src/test/Map.test.js
+++ webapp/src/test/Map.test.js
@@ -3,7 +3,7 @@ import {render, screen, fireEvent, cleanup, pwgetByText, waitFor}  from "@testin
 import "globalthis/auto";
 import ReactDOM from "react-dom";
 import Map from "../components/map/Map";
-import { MapMarker } from "../components/map/MapMarker";
+import { MapMarker, CoolState } from "../components/map/MapMarker";
 import "@testing-library/jest-dom/extend-expect";
 import {toRadianes, distanceFilter, radius} from "../components/map/LocationFunctions";
 
@@ -27,11 +27,17 @@ test ("check that locationFunctions work properly", async () => {
     var rad = toRadianes(180);
     assert.equal(rad, Math.PI);
     var user = {latitud : 0, longitud : 0};
-    console.log(distanceFilter(30, 30, user));
-    console.log(distanceFilter(0.001, 0.0001, user));
     user.latitud;
     var lng1 = user.longitud;
     assert.equal(distanceFilter(30, 30, user), false);  //fuera del radio
     assert.equal(distanceFilter(0.01, 0.01, user), true);   //dentro del radio
     
+});
+
+test("check that coolstate doesnt crash", async () => {
+    CoolState("comer");
+    CoolState("deporte");
+    CoolState("cita");
+    CoolState("covid");
+    CoolState("deporte");
 });
